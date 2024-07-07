61c6df9abf4885976f1fd6e7655308506aaa4cf9
NoobSterDev <76748114+nobelleon@users.noreply.github.com>
2024-07-07 19:49:26 +0700

    Add files via upload

---
+++ riverpod_architecture/lib/repository/home_repository.dart
@@ -0,0 +1,18 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+
+class HomeRepository {
+  //-------------
+  // weather api
+  //-------------
+  Future<http.Response> getWeatherData(
+      {required String latitude, required String longitude}) async {
+    final response = await http.get(
+      Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=e9bfb2065874c41e9fa86bd17cc74756"),
+    );
+    log("Weather Data : ${response.body}");
+    return response;
+  }
+}
