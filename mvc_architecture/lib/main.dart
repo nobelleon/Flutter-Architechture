e92f3f3d4ec07fdbbe404506faddc36325d7d6cb
NoobSterDev <76748114+nobelleon@users.noreply.github.com>
2024-07-07 19:30:29 +0700

    Add files via upload

---
+++ mvc_architecture/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:mvc_architecture/home/view/beranda.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MVC Architecture',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Beranda(),
+    );
+  }
+}
